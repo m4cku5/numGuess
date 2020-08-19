807e06c37d84abb1b0e3489fe80143b80baefd2e
m4cku5 <monsieur.mattingly@gmail.com>
2020-08-19 15:15:16 -0400

    Incremental improvements.

--- CLI/main.c
+++ CLI/main.c
@@ -1,9 +1,11 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-void compareNumbers(int *, int *, int *);
+void compareNumbers(int *, int *, bool *);
+void countGuesses(int *, int *);
 void getNumberOfGuesses(int *);
 void getRandomNumber(int *, int *, int *);
 void getRange(int *, int *);
@@ -11,70 +13,88 @@ void getGuess(int *, int *, int *);
 
 int main(int argc, char *argv[]) {
     
-    int guess;
-    int lowerNumber;
-    int numberOfGuesses;
-    int randomNumber;
-    int upperNumber;
+    bool running = true;
 
-    getRange(&lowerNumber, &upperNumber);
-    printf("You chose %d to %d as your range.\n", lowerNumber, upperNumber);
+    while(running) {
 
-    getRandomNumber(&lowerNumber, &upperNumber, &randomNumber);
-    printf("The random number is %d.\n", randomNumber);
+        int guess;
+        int guessCount = 1;
+        int lowerNumber;
+        int numberOfGuesses;
+        int randomNumber;
+        int upperNumber;
 
-    getNumberOfGuesses(&numberOfGuesses);
-    printf("You have %d guesses.\n", numberOfGuesses);
+        getRange(&lowerNumber, &upperNumber);
+        printf("You chose %d to %d as your range.\n", lowerNumber, upperNumber);
 
-    getGuess(&lowerNumber, &upperNumber, &guess);
-    printf("You guessed %d.\n", guess);
+        getRandomNumber(&lowerNumber, &upperNumber, &randomNumber);
+        //printf("The random number is %d.\n", randomNumber);
 
-    compareNumbers(&randomNumber, &guess, &numberOfGuesses);
+        getNumberOfGuesses(&numberOfGuesses);
+        printf("You have %d guesses.\n", numberOfGuesses);
+
+        while(guessCount <= numberOfGuesses) {
+
+            bool numbersMatch = false;
+            char t[7];
+
+            getGuess(&lowerNumber, &upperNumber, &guess);
+            printf("You guessed %d.\n", guess);
+
+            guessCount++;
+
+            compareNumbers(&randomNumber, &guess, &numbersMatch);
+
+            if(numbersMatch == true) {
+
+                printf("You guessed the correct number!\n");
+
+                if (guessCount < 2) {
+
+                    strncpy(t, "guess", 7);
+
+                } else {
+
+                    strncpy(t, "guesses", 7);
+
+                }
+
+                printf("It took you %d %s to get it right!\n", guessCount, t);
+
+                break;
+
+            } else {
+
+                printf("You guessed incorrectly!\n");
+
+            }
+
+        }
+
+        running = false;
+
+    }
 
     return 0;
 
 }
 
-void compareNumbers(int *a, int *b, int *c) {
+// randomGuess, guess, numbersMatch
+void compareNumbers(int *a, int *b, bool *c) {
 
-    for(int i = 0; i <= *c; i++) {
+    if(*a == *b) {
 
-        if(*a == *b) {
+        *c = true;
 
-            char t[7];
+    } else {
 
-            i++;
-
-            printf("You guessed the correct number!\n");
-
-            if (i < 2) {
-
-                strncpy(t, "guess", 7);
-
-            } else {
-
-                strncpy(t, "guesses", 7);
-
-            }
-
-            printf("It took you %d %s to get it right!\n", i, t);
-
-            break;
-
-        } else {
-
-            i++;
-
-            printf("Incorrect!\n");
-
-            break;
-
-        }
+        *c = false;
 
     }
 
 }
 
+// numberOfGuesses.
 void getNumberOfGuesses(int *a) {
 
     printf("How many guesses would you like?\t");
@@ -82,6 +102,7 @@ void getNumberOfGuesses(int *a) {
 
 }
 
+// lowerNumber, upperNumber, randomNumber
 void getRandomNumber(int *a, int *b, int *c) {
     
     srand(time(0));
@@ -90,6 +111,7 @@ void getRandomNumber(int *a, int *b, int *c) {
 
 }
 
+// lowerNumber, upperNumber
 void getRange(int *a, int *b) {
     
     printf("Choose the lowest number in the range.\t");
@@ -100,6 +122,7 @@ void getRange(int *a, int *b) {
 
 }
 
+// lowerNumber, upperNumber, guess
 void getGuess(int *a, int *b, int *c) {
 
     printf("Choose a number between %d and %d.\t", *a, *b);
